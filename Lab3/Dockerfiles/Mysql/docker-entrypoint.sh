d3ff225163509623e128538fe607b8fda3e6f995
Christian Kohout <christian.kohout@gmx.at>
2019-11-20 11:15:30 +0100

    Finished the mysql image

--- Lab3/Dockerfiles/Mysql/docker-entrypoint.sh
+++ Lab3/Dockerfiles/Mysql/docker-entrypoint.sh
@@ -3,8 +3,8 @@
 chown -R mysql:mysql /var/lib/mysql /var/run/mysqld
 
 sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mysql/my.cnf
-sed -i "s/dbname/$DB_NAME/g" /docker-entrypoint-initdb.d/init-database.sql
-sed -i "s/dbuser/$DB_USER/g" /docker-entrypoint-initdb.d/init-database.sql
-sed -i "s/dbpassword/$DB_PASSWORD/g" /docker-entrypoint-initdb.d/init-database.sql
+sed -i "s/dbname/$MYSQL_DATABASE/g" /docker-entrypoint-initdb.d/init-database.sql
+sed -i "s/dbuser/$MYSQL_USER/g" /docker-entrypoint-initdb.d/init-database.sql
+sed -i "s/dbpassword/$MYSQL_PASSWORD/g" /docker-entrypoint-initdb.d/init-database.sql
 
 mysqld --init-file="/docker-entrypoint-initdb.d/init-database.sql"
